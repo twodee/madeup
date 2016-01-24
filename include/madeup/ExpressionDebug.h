384c2a9170e166e12ed5f3e6dfb07134c43b1f11
Chris Johnson <johnch@uwec.edu>
2016-01-24 15:50:29 -0600

    Updates debug to handle same types as print.

--- include/madeup/ExpressionDebug.h
+++ include/madeup/ExpressionDebug.h
@@ -11,6 +11,9 @@ class ExpressionDebug : public Expression {
   public:
     ExpressionDebug();
 
+    static bool print(const std::string &label,
+                      const Co<Expression>& value);
+
     Co<Expression> evaluate(Environment &env) const;
     void write(ostream &out) const;
 
--- src/madeup/ExpressionDebug.cpp
+++ src/madeup/ExpressionDebug.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+#include "madeup/ExpressionArray.h"
 #include "madeup/ExpressionBoolean.h"
 #include "madeup/ExpressionClosure.h"
 #include "madeup/ExpressionDebug.h"
 #include "madeup/ExpressionInteger.h"
+#include "madeup/ExpressionPrint.h"
 #include "madeup/ExpressionReal.h"
 #include "madeup/ExpressionString.h"
-#include "madeup/ExpressionString.h"
+#include "madeup/ExpressionUnit.h"
 #include "twodee/MessagedException.h"
 
 using namespace td;
@@ -21,6 +23,57 @@ ExpressionDebug::ExpressionDebug() :
 
 /* ------------------------------------------------------------------------- */
 
+bool ExpressionDebug::print(const std::string &label, const Co<Expression>& value) {
+  const ExpressionString *string_value = dynamic_cast<const ExpressionString *>(value.GetPointer());
+  if (string_value) {
+    std::cout << label << ": " << string_value->getString();
+    return true;
+  }
+
+  const ExpressionReal *decimal_value = dynamic_cast<const ExpressionReal *>(value.GetPointer());
+  if (decimal_value) {
+    std::cout << label << ": " << decimal_value->toReal();
+    return true;
+  }
+
+  const ExpressionInteger *integer_value = dynamic_cast<const ExpressionInteger *>(value.GetPointer());
+  if (integer_value) {
+    std::cout << label << ": " << integer_value->toInteger();
+    return true;
+  }
+
+  const ExpressionBoolean *boolean_value = dynamic_cast<const ExpressionBoolean *>(value.GetPointer());
+  if (boolean_value) {
+    std::cout << label << ": " << (boolean_value->toBoolean() ? "true" : "false");
+    return true;
+  }
+
+  const ExpressionUnit *unit_value = dynamic_cast<const ExpressionUnit *>(value.GetPointer());
+  if (unit_value) {
+    std::cout << label << ": " << "nothing";
+    return true;
+  }
+
+  const ExpressionArrayReference *array_reference = dynamic_cast<const ExpressionArrayReference *>(value.GetPointer());
+  if (array_reference) {
+    Co<ExpressionArray> array_value = array_reference->getArray();
+    std::cout << label << ": " << "{";
+    if (array_value->getSize() > 0) {
+      ExpressionPrint::print((*array_value)[0]);
+      for (int i = 1; i < array_value->getSize(); ++i) {
+        std::cout << ", ";
+        ExpressionPrint::print((*array_value)[i]);
+      }
+    }
+    std::cout << "}";
+    return true;
+  }
+
+  return false;
+}
+
+/* ------------------------------------------------------------------------- */
+
 Co<Expression> ExpressionDebug::evaluate(Environment &env) const {
   Co<ExpressionClosure> message = env["message"];
 
@@ -29,32 +82,14 @@ Co<Expression> ExpressionDebug::evaluate(Environment &env) const {
   }
 
   Co<Expression> value = message->evaluate(env);
+  bool isPrintable = print(message->getSource(), value);
 
-  ExpressionString *string_value = dynamic_cast<ExpressionString *>(value.GetPointer());
-  if (string_value) {
-    std::cerr << message->getSource() << ": " << string_value->getString() << std::endl;
+  if (isPrintable) {
+    std::cout << std::endl;
     return value;
+  } else {
+    throw MessagedException(message->getSourceLocation().toAnchor() + ": I expect function debug to be given a string, boolean, number, or array. You gave it something I don't know how to print.");
   }
-
-  ExpressionReal *decimal_value = dynamic_cast<ExpressionReal *>(value.GetPointer());
-  if (decimal_value) {
-    std::cerr << message->getSource() << ": " << decimal_value->toReal() << std::endl;
-    return value;
-  }
-
-  ExpressionInteger *integer_value = dynamic_cast<ExpressionInteger *>(value.GetPointer());
-  if (integer_value) {
-    std::cerr << message->getSource() << ": " << integer_value->toInteger() << std::endl;
-    return value;
-  }
-
-  ExpressionBoolean *boolean_value = dynamic_cast<ExpressionBoolean *>(value.GetPointer());
-  if (boolean_value) {
-    std::cerr << message->getSource() << ": " << (boolean_value->toBoolean() ? "true" : "false") << std::endl;
-    return value;
-  }
-  
-  throw MessagedException(message->getSourceLocation().toAnchor() + ": I expect function debug to be given a string, boolean, or number. You gave it something I don't know how to print.");
 }
 
 /* ------------------------------------------------------------------------- */
