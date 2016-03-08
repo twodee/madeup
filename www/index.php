29f1b4410cc43415a4001712e8ba125a5dcc9fe4
Chris Johnson <johnch@uwec.edu>
2016-03-08 13:35:50 -0600

    Disallows user scaling.

--- www/index.php
+++ www/index.php
@@ -3,6 +3,8 @@
 <head>
   <title>Madeup</title>
 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
