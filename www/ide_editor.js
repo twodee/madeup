fe968548f096f1db2bc32b7ab9723d0e4458aa8a
Chris Johnson <johnch@uwec.edu>
2016-03-08 14:10:02 -0600

    Adds keyboard shortcuts for path, solid, and save.

--- www/ide_editor.js
+++ www/ide_editor.js
@@ -8,3 +8,30 @@ textEditor.setSession(ace.createEditSession('', 'ace/mode/madeup'));
 textEditor.setTheme("ace/theme/twilight");
 textEditor.setHighlightSelectedWord(false);
 textEditor.setHighlightActiveLine(false);
+
+textEditor.commands.addCommand({
+  'name' : 'solidify',
+  'bindKey' : { 'win' : 'Ctrl-,', 'mac' : 'Ctrl-,' },
+  'exec' : function(editor) {
+    run(getSource(), GeometryMode.SURFACE);
+  },
+  'readOnly' : true
+});
+
+textEditor.commands.addCommand({
+  'name' : 'pathify',
+  'bindKey' : { 'win' : 'Ctrl-.', 'mac' : 'Ctrl-.' },
+  'exec' : function(editor) {
+    run(getSource(), GeometryMode.PATH);
+  },
+  'readOnly' : true
+});
+
+textEditor.commands.addCommand({
+  'name' : 'save',
+  'bindKey' : { 'win' : 'Ctrl-S', 'mac' : 'Ctrl-S' },
+  'exec' : function(editor) {
+    save();
+  },
+  'readOnly' : true
+});
