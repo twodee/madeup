bd4c0fb9dfa32cf8339c686cd559df920e33b9c1
Chris Johnson <crjjrc@gmail.com>
2018-03-01 06:14:00 -0600

    Add tree lesson.

--- electron/bridge.js
+++ electron/bridge.js
@@ -11,7 +11,7 @@ function platformize() {
   $('#panel-section-file').remove();  
   $('#panel-section-mups').remove();  
 
-  lesson = 'marble';
+  lesson = 'tree';
 }
 
 function interpret(options, onSuccess, onError) {
