a727a2cc9d2fb25c7c6f55382b2fc490cd4698dc
Chris Johnson <crjjrc@gmail.com>
2018-03-12 07:48:29 -0500

    Add blinky lesson.

--- electron/bridge.js
+++ electron/bridge.js
@@ -11,7 +11,7 @@ function platformize() {
   $('#panel-section-file').remove();  
   $('#panel-section-mups').remove();  
 
-  lesson = 'tree';
+  lesson = 'blinky';
 }
 
 function interpret(options, onSuccess, onError) {
